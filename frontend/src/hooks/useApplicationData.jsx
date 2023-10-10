b124b74a3a66ddce482ea336c7dc281f4bc6166f
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-10-10 01:59:30 -0700

    Store favorite photos locally

--- frontend/src/hooks/useApplicationData.jsx
+++ frontend/src/hooks/useApplicationData.jsx
@@ -1,10 +1,11 @@
-import { useReducer, useEffect } from "react";
+import { useReducer, useEffect, useRef } from "react";
 
 const ACTIONS = {
   SET_PHOTO_DATA: 'SET_PHOTO_DATA',
   SET_TOPIC_DATA: 'SET_TOPIC_DATA',
   SHOW_PHOTO_DETAILS: 'SHOW_PHOTO_DETAILS',
   REMOVE_PHOTO_DETAILS: 'REMOVE_PHOTO_DETAILS',
+  SET_FAVORITE_PHOTOS: 'SET_FAVORITE_PHOTOS',
   TOGGLE_FAVORITE: 'TOGGLE_FAVORITE'
 };
 
@@ -21,6 +22,9 @@ function reducer(state, action) {
 
     case ACTIONS.REMOVE_PHOTO_DETAILS:
       return { ...state, selectedPhoto: null };
+
+    case ACTIONS.SET_FAVORITE_PHOTOS:
+      return { ...state, favoritePhotos: action.value };
 
     case ACTIONS.TOGGLE_FAVORITE:
       const isFavorite = state.favoritePhotos.find((photo) => photo.id === action.id);
@@ -45,6 +49,35 @@ function reducer(state, action) {
 }
 
 
+// GENERIC FETCH/STORE FUNCTION
+const fetchData = (url, localStorageKey, setDataFunction) => {
+  const storedData = localStorage.getItem(localStorageKey);
+  if (storedData) {
+    setDataFunction(JSON.parse(storedData));
+    return;
+  }
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      setDataFunction(data);
+      localStorage.setItem(localStorageKey, JSON.stringify(data));
+    })
+    .catch(error => {
+      console.log(`Error fetching data from "${url}": ${error}`);
+    });
+};
+
+
+// Keys for Local Storage
+const STORAGE_KEYS = {
+  PHOTOS_FOR_TOPIC: "PHOTOS_FOR_TOPIC",
+  ALL_PHOTOS: "ALL_PHOTOS",
+  TOPICS: "TOPICS",
+  FAVORITE_PHOTOS: 'FAVORITE_PHOTOS'
+};
+
+
 const initialState = {
   photoData: [],
   topicData: [],
@@ -52,82 +85,50 @@ const initialState = {
   favoritePhotos: [],
 };
 
-const STORAGE_KEYS = {
-  PHOTOS_FOR_TOPIC: "PHOTOS_FOR_TOPIC",
-  ALL_PHOTOS: "ALL_PHOTOS",
-  TOPICS: "TOPICS"
-};
-
+// CUSTOM HOOK
 const useApplicationData = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const justMounted = useRef(true);
 
   // Fetch and store all photos
   useEffect(() => {
-    const storedPhotos = localStorage.getItem(STORAGE_KEYS.ALL_PHOTOS);
-    if (storedPhotos) {
-      setPhotoData(JSON.parse(storedPhotos));
-      return;
-    }
-
-    fetch("/api/photos")
-      .then(res => res.json())
-      .then(photos => {
-        setPhotoData(photos);
-        localStorage.setItem(STORAGE_KEYS.ALL_PHOTOS, JSON.stringify(photos));
-      })
-      .catch(error => {
-        console.log(`Error fetching photos: ${error}`);
-      });
+    fetchData("/api/photos", STORAGE_KEYS.ALL_PHOTOS, setPhotoData);
   }, []);
 
 
   // Fetch and store topics
   useEffect(() => {
-    const storedTopics = localStorage.getItem(STORAGE_KEYS.TOPICS);
-    if (storedTopics) {
-      setTopicData(JSON.parse(storedTopics));
-      return;
-    }
-
-    fetch("/api/topics")
-      .then(res => res.json())
-      .then(topics => {
-        setTopicData(topics);
-        localStorage.setItem(STORAGE_KEYS.TOPICS, JSON.stringify(topics));
-      })
-      .catch(error => {
-        console.log(`Error fetching topics: ${error}`);
-      });
+    fetchData("/api/topics", STORAGE_KEYS.TOPICS, setTopicData);
   }, []);
 
 
-  // Methods to easily access dispatch functions
-  const setPhotoData = (value) => dispatch({ type: ACTIONS.SET_PHOTO_DATA, value });
-  const setTopicData = (value) => dispatch({ type: ACTIONS.SET_TOPIC_DATA, value });
-  const showPhotoDetails = (value) => dispatch({ type: ACTIONS.SHOW_PHOTO_DETAILS, value });
-  const removePhotoDetails = () => dispatch({ type: ACTIONS.REMOVE_PHOTO_DETAILS });
+  // Load favorite photos from storage
+  useEffect(() => {
+    const storedFavorites = localStorage.getItem(STORAGE_KEYS.FAVORITE_PHOTOS);
 
-  const setPhotosByTopics = (id) => {
-    const storedPhotosForTopic = localStorage.getItem(`${STORAGE_KEYS.PHOTOS_FOR_TOPIC}_${id}`);
-    if (storedPhotosForTopic) {
-      console.log();
-      setPhotoData(JSON.parse(storedPhotosForTopic));
+    // Load favorites only when the component mounts
+    if (justMounted.current && storedFavorites) {
+      setFavoritePhotos(JSON.parse(storedFavorites));
+      justMounted.current = false;
       return;
     }
 
-    fetch(`/api/topics/photos/${id}`)
-      .then(res => res.json())
-      .then(photosForTopic => {
-        setPhotoData(photosForTopic);
-        localStorage.setItem(
-          `${STORAGE_KEYS.PHOTOS_FOR_TOPIC}_${id}`, JSON.stringify(photosForTopic)
-        );
-      })
-      .catch(error => {
-        console.log(`Error fetching photos for topic id ${id}: ${error}`);
-      });
-  };
+    // Store favorites only when there's a change
+    if (storedFavorites !== JSON.stringify(state.favoritePhotos)) {
+      localStorage.setItem(STORAGE_KEYS.FAVORITE_PHOTOS, JSON.stringify(state.favoritePhotos));
+    }
+  }, [state.favoritePhotos]);
 
+
+  // CONVENIENCE METHODS FOR DISPATCHING ACTIONS
+  const setPhotoData = (value) => dispatch({ type: ACTIONS.SET_PHOTO_DATA, value });
+  const setTopicData = (value) => dispatch({ type: ACTIONS.SET_TOPIC_DATA, value });
+  const setFavoritePhotos = (value) => dispatch({ type: ACTIONS.SET_FAVORITE_PHOTOS, value });
+  const showPhotoDetails = (value) => dispatch({ type: ACTIONS.SHOW_PHOTO_DETAILS, value });
+  const removePhotoDetails = () => dispatch({ type: ACTIONS.REMOVE_PHOTO_DETAILS });
+  const setPhotosByTopics = (id) => {
+    fetchData(`/api/topics/photos/${id}`, `${STORAGE_KEYS.PHOTOS_FOR_TOPIC}_${id}`, setPhotoData);
+  };
   const toggleFavorite = (id) => dispatch({ type: ACTIONS.TOGGLE_FAVORITE, id });
 
 
